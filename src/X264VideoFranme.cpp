77d94b3756a8960f74d4cbc8b8b67aa5adea772e
Martin German <martin.german@i2cat.net>
2014-05-19 15:09:43 +0200

    TVRING-234: first release

---
+++ src/X264VideoFranme.cpp
@@ -0,0 +1,28 @@
+/*
+ *  X264VideoFrame - X264 Video frame structure
+ *  Copyright (C) 2013  Fundació i2CAT, Internet i Innovació digital a Catalunya
+ *
+ *  This file is part of media-streamer.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Authors: Martin German <martin.german@i2cat.net>
+ */
+
+#include "X264VideoFrame.hh"
+
+void X264VideoFrame::setNals(x264_nal_t **nals, int size){
+	ppNals = nals;
+	sizeNals = size;
+}

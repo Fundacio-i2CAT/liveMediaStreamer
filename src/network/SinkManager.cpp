125373e903937c5804ebc9e561e54bea46820b7b
David Cassany <david.cassany@i2cat.net>
2014-06-10 17:35:06 +0200

    Sink Manager recovered

--- src/network/SinkManager.cpp
+++ src/network/SinkManager.cpp
@@ -1,243 +1,240 @@
-// /*
-//  *  SinkManager.cpp - Class that handles multiple server sessions dynamically.
-//  *  Copyright (C) 2014  Fundació i2CAT, Internet i Innovació digital a Catalunya
-//  *
-//  *  This file is part of liveMediaStreamer.
-//  *
-//  *  This program is free software: you can redistribute it and/or modify
-//  *  it under the terms of the GNU General Public License as published by
-//  *  the Free Software Foundation, either version 3 of the License, or
-//  *  (at your option) any later version.
-//  *
-//  *  This program is distributed in the hope that it will be useful,
-//  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  *  GNU General Public License for more details.
-//  *
-//  *  You should have received a copy of the GNU General Public License
-//  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//  *
-//  *  Authors:  David Cassany <david.cassany@i2cat.net>,
-//  *            
-//  */
+/*
+ *  SinkManager.cpp - Class that handles multiple server sessions dynamically.
+ *  Copyright (C) 2014  Fundació i2CAT, Internet i Innovació digital a Catalunya
+ *
+ *  This file is part of liveMediaStreamer.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Authors:  David Cassany <david.cassany@i2cat.net>,
+ *            
+ */
 
 
-// #include "SinkManager.hh"
-// #include "H264QueueServerMediaSubsession.hh"
-// #include "../AVFramedQueue.hh"
-// #include "../AudioCircularBuffer.hh"
+#include "SinkManager.hh"
 
-// #ifndef _HANDLERS_HH
-// #include "Handlers.hh"
-// #endif
+#ifndef _HANDLERS_HH
+#include "Handlers.hh"
+#endif
 
-// SinkManager *SinkManager::mngrInstance = NULL;
+SinkManager *SinkManager::mngrInstance = NULL;
 
-// SinkManager::SinkManager(int readersNum): watch(0), TailFilter(readersNum)
-// {    
-//     TaskScheduler* scheduler = BasicTaskScheduler::createNew();
-//     this->env = BasicUsageEnvironment::createNew(*scheduler);
+SinkManager::SinkManager(): watch(0), TailFilter(readersNum)
+{    
+    TaskScheduler* scheduler = BasicTaskScheduler::createNew();
+    this->env = BasicUsageEnvironment::createNew(*scheduler);
     
-//     //TODO: Add authentication security
-//     rtspServer = RTSPServer::createNew(*env, RTSP_PORT, NULL);
-//     if (rtspServer == NULL) {
-//         *env << "Failed to create RTSP server: " << env->getResultMsg() << "\n";
-//     }
+    //TODO: Add authentication security
+    rtspServer = RTSPServer::createNew(*env, RTSP_PORT, NULL);
+    if (rtspServer == NULL) {
+        *env << "Failed to create RTSP server: " << env->getResultMsg() << "\n";
+    }
     
-//     OutPacketBuffer::increaseMaxSizeTo(MAX_VIDEO_FRAME_SIZE);
+    OutPacketBuffer::increaseMaxSizeTo(MAX_VIDEO_FRAME_SIZE);
     
-//     mngrInstance = this;
-// }
+    mngrInstance = this;
+}
 
-// SinkManager* 
-// SinkManager::getInstance(){
-//     if (mngrInstance != NULL){
-//         return mngrInstance;
-//     }
-//     srand(time(NULL));
-//     return new SinkManager();
-// }
+SinkManager* 
+SinkManager::getInstance(){
+    if (mngrInstance != NULL){
+        return mngrInstance;
+    }
+    srand(time(NULL));
+    return new SinkManager();
+}
 
-// void SinkManager::closeManager()
-// {
-//     watch = 1;
-//     if (mngrTh.joinable()){
-//         mngrTh.join();
-//     }
-// }
+void SinkManager::closeManager()
+{
+    watch = 1;
+    if (mngrTh.joinable()){
+        mngrTh.join();
+    }
+}
 
-// void SinkManager::destroyInstance()
-// {
-//     //TODO:
-// }
+void SinkManager::destroyInstance()
+{
+    //TODO:
+}
 
-// void *startServer(void *args)
-// {
-//     char* watch = (char*) args;
-//     SinkManager* instance = SinkManager::getInstance();
+void *startServer(void *args)
+{
+    char* watch = (char*) args;
+    SinkManager* instance = SinkManager::getInstance();
     
-//     if (instance == NULL || instance->envir() == NULL){
-//         return NULL;
-//     }
-//     instance->envir()->taskScheduler().doEventLoop(watch); 
+    if (instance == NULL || instance->envir() == NULL){
+        return NULL;
+    }
+    instance->envir()->taskScheduler().doEventLoop(watch); 
     
-//     delete &instance->envir()->taskScheduler();
-//     instance->envir()->reclaim();
+    delete &instance->envir()->taskScheduler();
+    instance->envir()->reclaim();
     
-//     return NULL;
-// }
+    return NULL;
+}
 
-// bool SinkManager::runManager()
-// {
-//     watch = 0;
-//     mngrTh = std::thread(std::bind(startServer, &watch));
-//     return mngrTh.joinable();
-// }
+bool SinkManager::runManager()
+{
+    watch = 0;
+    mngrTh = std::thread(std::bind(startServer, &watch));
+    return mngrTh.joinable();
+}
 
 
-// bool SinkManager::isRunning()
-// {
-//     return mngrTh.joinable();
-// }
+bool SinkManager::isRunning()
+{
+    return mngrTh.joinable();
+}
 
-// bool SinkManager::addSession(std::string id, std::vector<int> readers, std::string info, std::string desc)
-// {
-//     if (sessionList.count(id) > 0){
-//         return false;
-//     }
+bool SinkManager::addSession(std::string id, std::vector<int> readers, std::string info, std::string desc)
+{
+    if (sessionList.count(id) > 0){
+        return false;
+    }
     
-//     ServerMediaSession* servSession
-//         = ServerMediaSession::createNew(*(envir()), id.c_str(), info.c_str(), desc.c_str());
+    ServerMediaSession* servSession
+        = ServerMediaSession::createNew(*(envir()), id.c_str(), info.c_str(), desc.c_str());
         
-//     if (servSession == NULL){
-//         return false;
-//     }
+    if (servSession == NULL){
+        return false;
+    }
     
-//     ServerMediaSubsession *subsession;
+    ServerMediaSubsession *subsession;
     
-//     for (auto & reader : readers){
-//         if ((subsession = createSubsessionByReader(getReader(reader))) != NULL){
-//             servSession->addSubsession(subsession);
-//         } else {
-//             //TODO: delete ServerMediaSession and previous subsessions
-//             return false;
-//         }
-//     }
+    for (auto & reader : readers){
+        if ((subsession = createSubsessionByReader(getReader(reader))) != NULL){
+            servSession->addSubsession(subsession);
+        } else {
+            //TODO: delete ServerMediaSession and previous subsessions
+            return false;
+        }
+    }
     
-//     sessionList[id] = servSession;
+    sessionList[id] = servSession;
     
-//     return true;
-// }
+    return true;
+}
 
-// ServerMediaSubsession *SinkManager::createVideoMediaSubsession(VCodecType codec, QueueSource *source)
-// {
-//     switch(codec){
-//         case H264:
-//             return H264QueueServerMediaSubsession::createNew(*(envir()), source, True);
-//             break;
-//         default:
-//             break;
-//     }
-//     return NULL;
-// }
+ServerMediaSubsession *SinkManager::createVideoMediaSubsession(VCodecType codec, QueueSource *source)
+{
+    switch(codec){
+        case H264:
+            return H264QueueServerMediaSubsession::createNew(*(envir()), source, True);
+            break;
+        default:
+            break;
+    }
+    return NULL;
+}
 
-// ServerMediaSubsession *SinkManager::createSubsessionByReader(Reader *reader)
-// {
-//     const VideoFrameQueue *vQueue;
-//     const AudioFrameQueue *aQueue;
-//     const AudioCircularBuffer *circularBuffer;
-//     QueueSource *source;
+ServerMediaSubsession *SinkManager::createSubsessionByReader(Reader *reader)
+{
+    const VideoFrameQueue *vQueue;
+    const AudioFrameQueue *aQueue;
+    const AudioCircularBuffer *circularBuffer;
+    QueueSource *source;
     
-//     if ((source = dynamic_cast<QueueSource*>(reader)) == NULL){
-//         return NULL;
-//     }
-//     if ((vQueue = dynamic_cast<const VideoFrameQueue*>(reader->getQueue())) != NULL){
-//         return createVideoMediaSubsession(vQueue->getCodec(), source);
-//     }
-//     if ((aQueue = dynamic_cast<const AudioFrameQueue*>(reader->getQueue())) != NULL){
-//         //TODO:
-//     }
-//     if ((circularBuffer = dynamic_cast<const AudioCircularBuffer*>(reader->getQueue())) != NULL){
-//         //TODO
-//     }
-//     return NULL;
-// }
+    if ((source = dynamic_cast<QueueSource*>(reader)) == NULL){
+        return NULL;
+    }
+    if ((vQueue = dynamic_cast<const VideoFrameQueue*>(reader->getQueue())) != NULL){
+        return createVideoMediaSubsession(vQueue->getCodec(), source);
+    }
+    if ((aQueue = dynamic_cast<const AudioFrameQueue*>(reader->getQueue())) != NULL){
+        //TODO:
+    }
+    if ((circularBuffer = dynamic_cast<const AudioCircularBuffer*>(reader->getQueue())) != NULL){
+        //TODO
+    }
+    return NULL;
+}
 
-// bool SinkManager::removeSession(std::string id)
-// {   
-//     if (sessionList.find(id) == sessionList.end()){
-//         envir()->setResultMsg("Failed, no session found with this id!\n");
-//         return false;
-//     }
+bool SinkManager::removeSession(std::string id)
+{   
+    if (sessionList.find(id) == sessionList.end()){
+        envir()->setResultMsg("Failed, no session found with this id!\n");
+        return false;
+    }
    
-//     //TODO: manage closure, should add RTSP reference?
+    //TODO: manage closure, should add RTSP reference?
     
-//     //RTSPServer::closeAllClientSessionsForServerMediaSession()
-//     //session->deleteAllSubsessions()
+    //RTSPServer::closeAllClientSessionsForServerMediaSession()
+    //session->deleteAllSubsessions()
     
-//     sessionList.erase(id);
+    sessionList.erase(id);
     
-//     return false;
-// }
+    return false;
+}
 
-// bool SinkManager::publishSession(std::string id)
-// {  
-//     if (sessionList.find(id) == sessionList.end()){
-//         envir()->setResultMsg("Failed, no session found with this id!\n");
-//         return false;
-//     }
+bool SinkManager::publishSession(std::string id)
+{  
+    if (sessionList.find(id) == sessionList.end()){
+        envir()->setResultMsg("Failed, no session found with this id!\n");
+        return false;
+    }
     
-//     if (rtspServer == NULL){
-//         return false;
-//     }
+    if (rtspServer == NULL){
+        return false;
+    }
     
-//     rtspServer->addServerMediaSession(sessionList[id]);
-//     char* url = rtspServer->rtspURL(sessionList[id]);
-//     UsageEnvironment& env = rtspServer->envir();
+    rtspServer->addServerMediaSession(sessionList[id]);
+    char* url = rtspServer->rtspURL(sessionList[id]);
+    UsageEnvironment& env = rtspServer->envir();
     
-//     env << "\n\nPlay this stream using the URL \"" << url << "\"\n";
-//     delete[] url;
+    env << "\n\nPlay this stream using the URL \"" << url << "\"\n";
+    delete[] url;
     
-//     return true;
-// }
+    return true;
+}
 
-// ServerMediaSession* SinkManager::getSession(std::string id)
-// {
-//     if(sessionList.find(id) != sessionList.end()){
-//         return sessionList[id];
-//     }
+ServerMediaSession* SinkManager::getSession(std::string id)
+{
+    if(sessionList.find(id) != sessionList.end()){
+        return sessionList[id];
+    }
     
-//     return NULL;
-// }
+    return NULL;
+}
 
-// Reader *SinkManager::setReader(int readerID, FrameQueue* queue)
-// {
-//     const VideoFrameQueue *vQueue;
-//     const AudioFrameQueue *aQueue;
-//     const AudioCircularBuffer *circularBuffer;
+Reader *SinkManager::setReader(int readerID, FrameQueue* queue)
+{
+    const VideoFrameQueue *vQueue;
+    const AudioFrameQueue *aQueue;
+    const AudioCircularBuffer *circularBuffer;
     
-//     if (readers.size() >= getMaxReaders() || readers.count(readerID) > 0 ) {
-//         return NULL;
-//     }
+    if (readers.size() >= getMaxReaders() || readers.count(readerID) > 0 ) {
+        return NULL;
+    }
     
-//     if ((vQueue = dynamic_cast<const VideoFrameQueue*>(queue)) != NULL){
-//         switch (vQueue->getCodec()){
-//             case H264:
-//                 readers[readerID] = H264QueueSource::createNew(*(envir()), vQueue);
-//                 return readers[readerID];
-//                 break;
-//             default:
-//                 break;
-//         }
-//     }
+    if ((vQueue = dynamic_cast<const VideoFrameQueue*>(queue)) != NULL){
+        switch (vQueue->getCodec()){
+            case H264:
+                readers[readerID] = H264QueueSource::createNew(*(envir()), vQueue);
+                return readers[readerID];
+                break;
+            default:
+                break;
+        }
+    }
     
-//     if ((aQueue = dynamic_cast<const AudioFrameQueue*>(queue)) != NULL){
-//         //TODO:
-//     }
+    if ((aQueue = dynamic_cast<const AudioFrameQueue*>(queue)) != NULL){
+        //TODO:
+    }
     
-//     if ((circularBuffer = dynamic_cast<const AudioCircularBuffer*>(queue)) != NULL){
-//         //TODO
-//     }
+    if ((circularBuffer = dynamic_cast<const AudioCircularBuffer*>(queue)) != NULL){
+        //TODO
+    }
 
-//     return NULL;
-// }
+    return NULL;
+}
--- src/network/SinkManager.hh
+++ src/network/SinkManager.hh
@@ -1,87 +1,84 @@
-// /*
-//  *  SinkManager.hh - Class that handles multiple server sessions dynamically.
-//  *  Copyright (C) 2014  Fundació i2CAT, Internet i Innovació digital a Catalunya
-//  *
-//  *  This file is part of liveMediaStreamer.
-//  *
-//  *  This program is free software: you can redistribute it and/or modify
-//  *  it under the terms of the GNU General Public License as published by
-//  *  the Free Software Foundation, either version 3 of the License, or
-//  *  (at your option) any later version.
-//  *
-//  *  This program is distributed in the hope that it will be useful,
-//  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  *  GNU General Public License for more details.
-//  *
-//  *  You should have received a copy of the GNU General Public License
-//  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//  *
-//  *  Authors:  David Cassany <david.cassany@i2cat.net>,
-//  *            
-//  */
-// #ifndef _SINK_MANAGER_HH
-// #define _SINK_MANAGER_HH
+/*
+ *  SinkManager.hh - Class that handles multiple server sessions dynamically.
+ *  Copyright (C) 2014  Fundació i2CAT, Internet i Innovació digital a Catalunya
+ *
+ *  This file is part of liveMediaStreamer.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Authors:  David Cassany <david.cassany@i2cat.net>,
+ *            
+ */
+#ifndef _SINK_MANAGER_HH
+#define _SINK_MANAGER_HH
 
-// #ifndef _BASIC_USAGE_ENVIRONMENT_HH
-// #include <BasicUsageEnvironment.hh>
-// #endif
+#ifndef _BASIC_USAGE_ENVIRONMENT_HH
+#include <BasicUsageEnvironment.hh>
+#endif
 
-// #ifndef _LIVEMEDIA_HH
-// #include <liveMedia/liveMedia.hh>
-// #endif
+#ifndef _LIVEMEDIA_HH
+#include <liveMedia/liveMedia.hh>
+#endif
 
-// #ifndef _QUEUE_SOURCE_HH
-// #include "QueueSource.hh"
-// #endif
+#ifndef _QUEUE_SOURCE_HH
+#include "QueueSource.hh"
+#endif
 
-// #include "../Filter.hh"
+#include <thread>
+#include <map>
+#include <string>
+#include <vector>
 
-// #include <thread>
-// #include <map>
-// #include <string>
-// #include <vector>
+#define RTSP_PORT 8554
 
-
-// #define RTSP_PORT 8554
-
-// class SinkManager : public TailFilter {
-// private:
-//     SinkManager(int readersNum = MAX_READERS);
+class SinkManager : public TailFilter {
+private:
+    SinkManager(int readersNum);
     
-// public:
-//     static SinkManager* getInstance();
-//     static void destroyInstance();
+public:
+    static SinkManager* getInstance();
+    static void destroyInstance();
 
-//     bool runManager();
-//     bool isRunning();
+    bool runManager();
+    bool isRunning();
     
-//     void closeManager();
+    void closeManager();
 
-//     bool addSession(std::string id, std::vector<int> readers, 
-//                     std::string info = NULL, std::string desc = NULL);
+    bool addSession(std::string id, std::vector<int> readers, 
+                    std::string info = NULL, std::string desc = NULL);
     
-//     ServerMediaSession* getSession(std::string id); 
-//     bool publishSession(std::string id);
-//     bool removeSession(std::string id);
+    ServerMediaSession* getSession(std::string id); 
+    bool publishSession(std::string id);
+    bool removeSession(std::string id);
     
-//     UsageEnvironment* envir() {return env;}
+    UsageEnvironment* envir() {return env;}
     
-// protected:
-//     Reader *setReader(int readerID, FrameQueue* queue);
+protected:
+    Reader *setReader(int readerID, FrameQueue* queue);
     
-// private: 
-//     ServerMediaSubsession *createSubsessionByReader(Reader *reader);
-//     ServerMediaSubsession *createVideoMediaSubsession(VCodecType codec, QueueSource *source);
+private: 
+    ServerMediaSubsession *createSubsessionByReader(Reader *reader);
+    ServerMediaSubsession *createVideoMediaSubsession(VCodecType codec, QueueSource *source);
     
-//     std::thread mngrTh;
+    std::thread mngrTh;
    
-//     static SinkManager* mngrInstance;
-//     std::map<std::string, ServerMediaSession*> sessionList;
-//     UsageEnvironment* env;
-//     uint8_t watch;
+    static SinkManager* mngrInstance;
+    std::map<std::string, ServerMediaSession*> sessionList;
+    UsageEnvironment* env;
+    uint8_t watch;
     
-//     RTSPServer* rtspServer;
-// };
+    RTSPServer* rtspServer;
+};
 
-// #endif
+#endif

53ab801005d574c194619b4f8c0e970f02aedb75
David Cassany <david.cassany@i2cat.net>
2015-05-13 15:33:04 +0200

    Updated worker

--- src/Worker.hh
+++ src/Worker.hh
@@ -37,31 +37,27 @@
 
 #define MAX_SLAVE 16
 
-/*! Worker class is a pure abstract class that is mostly in charge of executing
-    the process method of one or several filters in a dedicated thread.
-*/
 class Worker {
-
+    
 public:
-
+    
     /**
     * Creates a worker object
     */
     Worker();
     virtual ~Worker();
-
+    
     /**
     * Starts worker thread
     * @return True if succeded and false if not
     */
     bool start();
-
+    
     /**
-    * Check worker running status
-    * @return True if running and false if not
+    * @return True if succeded and false if not
     */
     bool isRunning();
-
+    
     /**
     * Tell its thread to stop and waits until done
     */
@@ -70,36 +66,34 @@ public:
     /**
     * Adds a processor to the worker
     * @param id processor ID
-    * @param processor object to add
+    * @param processor processor objecto to add
     * @return True if succeded and false if not
     */
     bool addProcessor(int id, Runnable *processor);
-
+    
     /**
     * Removes a processor from the worker's list
     * @param id processor ID
     * @return True if succeded and false if not
     */
     bool removeProcessor(int id);
-
+    
     /**
-    * Get worker type
     * @return returns worker's type, see Types.hh
     */
     WorkerType getType(){return type;};
-
+    
     /**
-    * Get the number of processors
-    * @return returns the number of current processors
-    */
-    size_t getProcessorsSize(){return processors.size();};
-
-    /**
-    * Fills the appropriate JSON state object
+    * @return returns the number of curent processors
     * @param workerNode json reference to fill
     */
+    size_t getProcessorsSize(){return processors.size();};
+    
+    /**
+    * Fills the appropriate JSON state object
+    */
     void getState(Jzon::Object &workerNode);
-
+    
 protected:
     virtual void process();
 
@@ -114,14 +108,7 @@ protected:
 
 class LiveMediaWorker : public Worker {
 public:
-    /**
-    * Creates a liveMediaWorker worker object, see worker types at Types.hh
-    */
     LiveMediaWorker();
-
-    /**
-    * Tell its thread to stop and waits until done
-    */
     void stop();
 private:
     void process();

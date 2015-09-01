123f9d2166a8a7c792fb0c84855abdb8368c1a2f
David Cassany <david.cassany@i2cat.net>
2015-09-01 15:32:45 +0200

    Updated lookahead control

--- src/modules/liveMediaOutput/SinkManager.cpp
+++ src/modules/liveMediaOutput/SinkManager.cpp
@@ -146,8 +146,12 @@ bool SinkManager::doProcessFrame(std::map<int, Frame*> &oFrames, std::vector<int
         }
     }
     
+    utils::infoMsg("Added frames: " + std::to_string(pFrames));
+    
     if (pFrames == 0){
+        utils::errorMsg("No frames");
         scheduler->SingleStep();
+        utils::errorMsg("ending");
         return false;
     }
     
@@ -159,6 +163,8 @@ bool SinkManager::doProcessFrame(std::map<int, Frame*> &oFrames, std::vector<int
             }
         }
     }
+    
+    utils::infoMsg("Consumed frames: " + std::to_string(pFrames));
 
     return true;
 }
